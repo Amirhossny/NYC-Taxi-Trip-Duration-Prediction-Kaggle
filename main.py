2d5e966ff245d98fe9e3a83ecee9d744c12bfe9d
Amir Hosni <amiirhosnii548@gmail.com>
2026-02-16 23:52:17 +0200

    fix eval

--- main.py
+++ main.py
@@ -6,12 +6,7 @@ from src.Preprocessing import preprocess_data
 from src.eval import evaluate
 from src.logger import setup_logging, log_metrics
 from src.Helper_Fun import load_data, load_config, save_model, load_model
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Ridge, Lasso
-from src.Feature_Engineering import FeatureEngineering
-import numpy as np
-import pandas as pd
-from sklearn.metrics import r2_score, mean_squared_error
+
 
 # -------- Paths --------
 BASE_DIR = Path(__file__).resolve().parent
@@ -85,15 +80,18 @@ def test_model(model_name, x_test, y_test, use_log):
 if __name__ == "__main__":
     
 
+    train_and_evaluate_model(model_name="ridge", x_train=x_train, y_train=y_train, x_val=x_val, y_val=y_val, use_log=use_log)
+    # trainer = ModelPipeline(config)
+    # trainer.fit(x_train, y_train, model_type="ridge")
     
-    trainer = ModelPipeline(config)
-    trainer.fit(x_train, y_train, model_type="ridge")
-    preds = trainer.predict(x_val)
-    rmse = np.sqrt(mean_squared_error(y_val, preds))
-    r2 = r2_score(y_val, preds)
+    # metrics = evaluate(x_val, y_val, trainer, name='ridge', use_log=use_log)
+    # print(metrics)
+    # preds = trainer.predict(x_val)
+    # rmse = np.sqrt(mean_squared_error(y_val, preds))
+    # r2 = r2_score(y_val, preds)
 
-    print(f"Ridge RMSE = {rmse:.4f} & R2 = {r2:.4f}")
+    # print(f"Ridge RMSE = {rmse:.4f} & R2 = {r2:.4f}")
     
-
+    
     
     
--- src/eval.py
+++ src/eval.py
@@ -2,17 +2,25 @@ import numpy as np
 from sklearn.metrics import r2_score, mean_squared_error
 
 
-def inverse_target(y_transformed, use_log=True):
+def evaluate(x, y, model, name, use_log=True):
+    preds = model.predict(x)
+
     if use_log:
-        return np.expm1(y_transformed)
-    return y_transformed
-
-def evaluate(X_test, y_test, model, name, use_log=True):
-    pred_trans = model.predict(X_test)
-    y_pred_real = inverse_target(pred_trans, use_log)
-    y_true_real = inverse_target(y_test, use_log)
-
-    rmse = np.sqrt(mean_squared_error(y_true_real, y_pred_real))
-    r2 = r2_score(y_true_real, y_pred_real)
-    # print(f"{name} RMSE = {rmse:.4f} & R2 = {r2:.4f}")
-    return {'r2': r2, 'rmse': rmse}
+        r2 = r2_score(y, preds)
+        rmse = np.sqrt(
+            mean_squared_error(
+                np.expm1(y),
+                np.expm1(preds)
+            )
+        )
+        return {
+            "r2_log": r2,
+            "rmse": rmse
+        }
+    else:
+        r2 = r2_score(y, preds)
+        rmse = np.sqrt(mean_squared_error(y, preds))
+        return {
+            "r2": r2,
+            "rmse": rmse
+        }
